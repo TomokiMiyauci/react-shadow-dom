4b7c85188a175a8261d3a560e2679abc1e001326
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-10-24 02:36:39 +0900

    refactor: remove unused

--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface TemplateProps {
-  shadowrootmode: "open" | "closed";
-  shadowrootclonable?: boolean;
-  shadowrootdelegatesfocus?: boolean;
-  shadowrootserializable?: boolean;
-}
